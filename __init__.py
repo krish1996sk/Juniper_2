34f84443b3f53e248d50e7f2c61aa5baa691d135
krish1996sk <krish1996sk@gmail.com>
2018-08-23 15:09:15 +0530

    commit

---
+++ __init__.py
@@ -0,0 +1,42 @@
+
+import re
+from mycroft.skills.core import MycroftSkill, intent_handler
+from mycroft.util.log import LOG
+from mycroft.util.log import getLogger
+from adapt.intent import IntentBuilder
+
+import json
+import requests
+
+
+LOGGER = getLogger(__name__)
+
+
+class Juniper_skill(MycroftSkill):
+    def __init__(self):
+        super(Juniper_skill, self).__init__(name="Juniper_skill")
+
+    @intent_handler(IntentBuilder("Juniper_device").require("Query").require("Juniper").require("input_id"))
+    def handle_intent(self, message):
+        # Extract what the user asked about
+        device_id = message.data.get("input_id")
+        #str1 = "good device"
+        output = self.call_api_output(device_id)
+
+
+        self.speak_dialog("device_status",{"status":output})
+
+    def call_api_output(self,device_id):
+        data  = requests.get("http://localhost:5000/devices/"+device_id)
+        json_data = data.json()
+        output = json_data['status']
+        return output
+
+
+
+
+
+
+
+def create_skill():
+    return Juniper_skill()
