4fd1d75f067c0f64ecac230104e3fe0343549373
krish1996sk <krish1996sk@gmail.com>
2018-08-27 12:13:01 +0530

    final commit

--- __init__.py
+++ __init__.py
@@ -16,10 +16,10 @@ class Juniper_skill(MycroftSkill):
     def __init__(self):
         super(Juniper_skill, self).__init__(name="Juniper_skill")
 
-    @intent_handler(IntentBuilder("Juniper_device").require("Query").require("Juniper").require("regex.rx"))
+    @intent_handler(IntentBuilder("Juniper_device").require("Query").require("Juniper").require("input_id"))
     def handle_intent(self, message):
         # Extract what the user asked about
-        device_id = message.data.get("regex.rx")
+        device_id = message.data.get("input_id")
         #str1 = "good device"
         output = self.call_api_output(device_id)
 
